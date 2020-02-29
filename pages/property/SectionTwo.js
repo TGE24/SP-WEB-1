f1f16b091b26870a652b83c07309bca16d568e77
cyuket <cyriluket12@gmail.com>
2020-02-29 11:54:37 +0100

    NEW PAGE I created a new page called propertie

---
+++ pages/property/SectionTwo.js
@@ -0,0 +1,176 @@
+import React from "react";
+import { Row, Col } from "antd";
+import { PropSectionTwo } from "./styled";
+
+export default () => {
+  return (
+    <PropSectionTwo>
+      <div className="row-head">
+        <h1>Houses for rent</h1>
+        <div className="sort-by">sksks</div>
+      </div>
+      <Row>
+        <Col xs={24} sm={2} md={6} lg={6} xl={6}>
+          <div className="prop-cards">
+            <div className="image">
+              <img src="../assets/prop1.png" alt="" />
+              <div className="apartment">
+                <h4>Apartment</h4>
+              </div>
+              <div className="feature">
+                <h4>Featured</h4>
+              </div>
+            </div>
+
+            <div className="prop-details">
+              <div className="inner-container">
+                <h2>Ancient Bungalo Ancient </h2>
+                <h4>
+                  <img src="../assets/icons/location.png" alt="" /> 45 ntoe asi
+                  layout , Calabar
+                </h4>
+              </div>
+            </div>
+            <div className="prop-icons">
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/house.png" alt="" /> 6
+                </h4>
+              </div>
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/bathroom.png" alt="" /> 6
+                </h4>
+              </div>
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/bed.png" alt="" /> 6
+                </h4>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={24} sm={2} md={6} lg={6} xl={6}>
+          <div className="prop-cards">
+            <div className="image">
+              <img src="../assets/prop2.png" alt="" />
+              <div className="apartment">
+                <h4>Apartment</h4>
+              </div>
+              <div className="feature">
+                <h4>Featured</h4>
+              </div>
+            </div>
+
+            <div className="prop-details">
+              <div className="inner-container">
+                <h2>Ancient Bungalo Ancient </h2>
+                <h4>
+                  <img src="../assets/icons/location.png" alt="" /> 45 ntoe asi
+                  layout , Calabar
+                </h4>
+              </div>
+            </div>
+            <div className="prop-icons">
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/house.png" alt="" /> 6
+                </h4>
+              </div>
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/bathroom.png" alt="" /> 6
+                </h4>
+              </div>
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/bed.png" alt="" /> 6
+                </h4>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={24} sm={2} md={6} lg={6} xl={6}>
+          <div className="prop-cards">
+            <div className="image">
+              <img src="../assets/prop3.png" alt="" />
+              <div className="apartment">
+                <h4>House</h4>
+              </div>
+              <div className="feature">
+                <h4>Featured</h4>
+              </div>
+            </div>
+
+            <div className="prop-details">
+              <div className="inner-container">
+                <h2>Ancient Bungalo Ancient </h2>
+                <h4>
+                  <img src="../assets/icons/location.png" alt="" /> 45 ntoe asi
+                  layout , Calabar
+                </h4>
+              </div>
+            </div>
+            <div className="prop-icons">
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/house.png" alt="" /> 6
+                </h4>
+              </div>
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/bathroom.png" alt="" /> 6
+                </h4>
+              </div>
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/bed.png" alt="" /> 6
+                </h4>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={24} sm={2} md={6} lg={6} xl={6}>
+          <div className="prop-cards">
+            <div className="image">
+              <img src="../assets/prop4.png" alt="" />
+              <div className="apartment">
+                <h4>House</h4>
+              </div>
+              <div className="feature">
+                <h4>Featured</h4>
+              </div>
+            </div>
+
+            <div className="prop-details">
+              <div className="inner-container">
+                <h2>Ancient Bungalo Ancient </h2>
+                <h4>
+                  <img src="../assets/icons/location.png" alt="" /> 45 ntoe asi
+                  layout , Calabar
+                </h4>
+              </div>
+            </div>
+            <div className="prop-icons">
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/house.png" alt="" /> 6
+                </h4>
+              </div>
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/bathroom.png" alt="" /> 6
+                </h4>
+              </div>
+              <div className="icon">
+                <h4>
+                  <img src="../assets/icons/bed.png" alt="" /> 6
+                </h4>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </PropSectionTwo>
+  );
+};
