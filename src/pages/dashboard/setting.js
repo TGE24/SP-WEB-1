8265f0e13b1b465951ec180bce2fc3ef5b4ee2b3
cyuket <cyriluket12@gmail.com>
2020-04-06 19:37:26 +0100

    New Screens

--- src/pages/dashboard/setting.js
+++ src/pages/dashboard/setting.js
@@ -1,6 +1,20 @@
 import React, { useState } from "react";
 import DashboardLayout from "../../components/Layout/dashboard";
-
+import "./dash.css";
 export default () => {
-  return <DashboardLayout title="Account Settings">Setting</DashboardLayout>;
+  return (
+    <DashboardLayout title="Account Settings">
+      <div className="container">
+        <div className="setting-banner">
+          <div className="background">
+            <div className="">
+              <h1>Erim, Anna Ndoma</h1>
+              <h2>annmikaerim@gmail.com</h2>
+            </div>
+          </div>
+          <div className="circle"></div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
